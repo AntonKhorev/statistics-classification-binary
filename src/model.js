ca931a944770a2a8704856a6b8b087153db52e05
Anton Khorev <tony29@yandex.ru>
2015-08-19 23:47:02 +0300

    training/test split prediction code with wrong comments

--- src/model.js
+++ src/model.js
@@ -9,22 +9,61 @@ Model.prototype.generateLines=function(){
 	var code=this.options.code;
 	var data=code.data;
 	var i18n=this.options.i18n;
-	return this.listLibraries().map(function(lib){
+	var lines=[];
+	lines=lines.concat(this.listLibraries().map(function(lib){
 		return "library("+lib+")";
-	}).concat([
+	}),[
 		"",
 		"# "+i18n('load data'),
 		e(data)+"=read.csv('"+e(code.filename)+"')",
-	],code.postprocess?[
-		"# "+i18n('postprocess data'),
-		e(code.postprocess),
-	]:[],code.needSplit?[
-		"# "+i18n('split data into training/test set'),
-		"set.seed("+e(code.splitSeed)+")",
-		"split=sample.split("+e(data)+"$"+e(code.y)+",SplitRatio="+e(code.splitRatio)+")",
-		e(data.train)+"="+e(data)+"[split,]",
-		e(data.test)+"="+e(data)+"[!split,]",
-	]:[],this.generateModelProbLines(code.needSplit?data.train:data),[
+	]);
+	if (code.postprocess) {
+		lines=lines.concat([
+			"# "+i18n('postprocess data'),
+			e(code.postprocess),
+		]);
+	}
+	var modelLines;
+	if (code.needSplit) {
+		lines=lines.concat([
+				"# "+i18n('split data into training/test set'),
+				"set.seed("+e(code.splitSeed)+")",
+				"split=sample.split("+e(data)+"$"+e(code.y)+",SplitRatio="+e(code.splitRatio)+")",
+				e(data.train)+"="+e(data)+"[split,]",
+				e(data.test)+"="+e(data)+"[!split,]",
+		]);
+		modelLines=this.generateModelLines(data.train);
+		if (modelLines.length) {
+			lines=lines.concat(["# "+i18n('build model')]);
+		}
+		lines=lines.concat(
+			modelLines,
+			["# "+i18n('training set probability prediction')],
+			this.generateProbLines(data.train),
+			this.generateClassLines(data.train),
+			["# "+i18n('test set probability prediction')],
+			this.generateProbLines(data.test),
+			this.generateClassLines(data.test)
+		);
+	} else {
+		modelLines=this.generateModelLines(data);
+		if (modelLines.length) {
+			lines=lines.concat(["# "+i18n('build model')]);
+		}
+		lines=lines.concat(
+			modelLines,
+			["# "+i18n('in-sample probability prediction')],
+			this.generateProbLines(data),
+			this.generateClassLines(data)
+		);
+	}
+	return lines;
+};
+Model.prototype.generateClassLines=function(data){
+	var e=this.options.encode;
+	var code=this.options.code;
+	var i18n=this.options.i18n;
+	return [
 		"# "+i18n('in-sample class prediction'),
 		e(data['class'])+"=+("+e(data.prob)+">="+e(code.threshold)+")",
 		"# "+i18n('in-sample accuracy'),
@@ -33,7 +72,7 @@ Model.prototype.generateLines=function(){
 		e(data.auc)+"=performance(",
 		"\t"+"prediction("+e(data.prob)+","+e(data)+"$"+e(code.y)+"),'auc'",
 		")@y.values[[1]]",
-	]);
+	];
 };
 Model.prototype.listLibraries=function(){
 	libs=[
@@ -52,13 +91,14 @@ var BaselineModel=function(options){
 BaselineModel.prototype=Object.create(Model.prototype);
 BaselineModel.prototype.constructor=BaselineModel;
 BaselineModel.prototype.name='Baseline method';
-BaselineModel.prototype.generateModelProbLines=function(data){
+BaselineModel.prototype.generateModelLines=function(data){
+	return []; // no model
+}
+BaselineModel.prototype.generateProbLines=function(data){
 	var e=this.options.encode;
 	var code=this.options.code;
 	var i18n=this.options.i18n;
 	return [
-		// no model
-		"# "+i18n('in-sample probability prediction'), // on complete dataset
 		e(data.prob)+"=rep_len(mean("+e(data)+"$"+e(code.y)+"),nrow("+e(data)+"))",
 	];
 };
@@ -69,14 +109,19 @@ var LogregModel=function(options){
 LogregModel.prototype=Object.create(Model.prototype);
 LogregModel.prototype.constructor=LogregModel;
 LogregModel.prototype.name='Logistic regression';
-LogregModel.prototype.generateModelProbLines=function(data){
+LogregModel.prototype.generateModelLines=function(data){
 	var e=this.options.encode;
 	var code=this.options.code;
 	var i18n=this.options.i18n;
 	return [
-		"# "+i18n('build model'),
 		e(data.model)+"=glm("+e(code.formula)+",data="+e(data)+",family=binomial)",
-		"# "+i18n('in-sample probability prediction'), // on complete dataset
+	];
+};
+LogregModel.prototype.generateProbLines=function(data){
+	var e=this.options.encode;
+	var code=this.options.code;
+	var i18n=this.options.i18n;
+	return [
 		e(data.prob)+"=predict("+e(data.model)+",type='response')",
 	];
 };
@@ -87,14 +132,19 @@ var CartModel=function(options){
 CartModel.prototype=Object.create(Model.prototype);
 CartModel.prototype.constructor=CartModel;
 CartModel.prototype.name='Regression tree';
-CartModel.prototype.generateModelProbLines=function(data){
+CartModel.prototype.generateModelLines=function(data){
 	var e=this.options.encode;
 	var code=this.options.code;
 	var i18n=this.options.i18n;
 	return [
-		"# "+i18n('build model'),
 		e(data.model)+"=rpart("+e(code.formula)+",data="+e(data)+")",
-		"# "+i18n('in-sample probability prediction'), // on complete dataset
+	];
+};
+CartModel.prototype.generateProbLines=function(data){
+	var e=this.options.encode;
+	var code=this.options.code;
+	var i18n=this.options.i18n;
+	return [
 		e(data.prob)+"=predict("+e(data.model)+")",
 	];
 };
